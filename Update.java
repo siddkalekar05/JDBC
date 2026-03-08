70b03f4480b3207394e7b2865a60324a3bae5f77
Siddhesh Kalekar <kalekarsiddhesh593@gmail.com>
2026-03-08 19:50:55 +0530

    Add files via upload

---
+++ Update.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+public class Update {
+    private static final String url="jdbc:mysql://localhost:3306/mydb";
+
+    private static final String username="root";
+
+    private static final String passsword = "SIDDhesh@09012005";
+    private static String query = "UPDATE student " +
+            "SET name = 'Rohit', marks = 7 " +
+            " WHERE id = 4";
+
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        try{
+            Connection c = DriverManager.getConnection(url,username,passsword);
+            Statement st = c.createStatement();
+            int rowsaffect = st.executeUpdate(query);
+            if(rowsaffect>0){
+                System.out.println("Successfully Updated "+rowsaffect);
+            }else {
+                System.out.println("Unsuccessfull");
+            }
+
+        }catch(SQLException S){
+            System.out.println(S.getMessage());
+        }
+    }
+}
